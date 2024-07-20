cbf1d6df9a11f06b086804159c7111feb8227ed4
Lekhnath Parajuli <lekhnathparajuli4@gmail.com>
2024-07-20 11:36:44 +0545

    added migration for user and contact

--- user/models.py
+++ user/models.py
@@ -1,3 +1,26 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+
+
+
+
+
+class User(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100, primary_key=False)
+    email = models.CharField(max_length=100, primary_key=False)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Contact(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100, primary_key=False)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=100, primary_key=False)
+
+    def __str__(self) -> str:
+        return self.phone_number
