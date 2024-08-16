4ceb23c0e12d43f5a9b4b3466b233b9cb00afc76
Lekhnath Parajuli <lekhnathparajuli4@gmail.com>
2024-08-16 13:19:19 +0545

    working on seed data

--- api/settings.py
+++ api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure--a2-ofb68vn$-0_1dx7r+==dtk-a$jsr46c@mbw+cl=nv-e0&%"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     "user",
     "search",
+    "django_seed",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
